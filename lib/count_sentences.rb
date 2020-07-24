ea397ce3625a3c116f5e22e1b64e37b5ff825758
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-24 00:16:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -24,7 +24,7 @@ class String
   def count_sentences
     sentences=[]
   #if self == (/[.?!]+/)
-    sentences=self.split (/[.?!]+/).reject {|
+    sentences=self.split (/[.?!]+/).reject {|string| string.empty?}.count
     
   #end
 end
